a0f6be9cf0a4947e767c5df989412b52508ba8fb
zardori <ignacy2038@gmail.com>
2024-03-10 17:49:52 +0100

    removed pycache and changed project name

--- web_apps_project/asgi.py
+++ web_apps_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for awww_project project.
+ASGI config for web_apps_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awww_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_apps_project.settings')
 
 application = get_asgi_application()
--- web_apps_project/wsgi.py
+++ web_apps_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for awww_project project.
+WSGI config for web_apps_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awww_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_apps_project.settings')
 
 application = get_wsgi_application()
